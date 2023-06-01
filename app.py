a547920e55601e1bb512a443f5fbc2d3ef565ee9
Dieter1978 <14483@coderacademy.edu.au>
2023-06-01 11:43:44 +1000

    messing with json and dbs

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
+from flask_marshmallow import Marshmallow
+import json
 
 db = SQLAlchemy()
 app = Flask(__name__)
@@ -8,6 +10,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spameggs123@localhost:5432/trello'
 
 db.init_app(app)
+ma = Marshmallow(app)
 
 
 class Card(db.Model):
@@ -16,6 +19,15 @@ class Card(db.Model):
     title = db.Column(db.String(250))
     description = db.Column(db.Text())
     date_created = db.Column(db.Date())
+
+
+class CardSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ("id", "title", "description", "date_created")
+
+
+cards_schema = CardSchema(many=True)
 
 
 @app.cli.command('create')
@@ -49,6 +61,21 @@ def seed_db():
 @app.route('/')
 def index():
     return 'Hello World!'
+
+
+# @app.route("/cards", methods=["GET"])
+# def get_cards():
+    # get all the cards from the database table
+ #   cards_list = Card.query.all()
+    # return the data in JSON format
+  #  result = cards_schema.dump(cards_list)
+  #  return jsonify(result)
+
+@app.route("/cards")
+def all_cards():
+    stmt = db.select(Card).order_by(Card.status.desc())
+    cards = db.session.scalars(stmt).all()
+    return json.dumps(cards)
 
 
 if __name__ == '__main__':
